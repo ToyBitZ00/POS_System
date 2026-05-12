edc9b939c3465501b67de565f4c4129577929c45
Paul Nerie <pa6308191@gmail.com>
2026-05-12 13:08:51 +0800

    Successfully configured ProductsController.

---
+++ app/Http/Controllers/ProductsController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Product;
+
+class ProductsController extends Controller
+{
+    public function index(){
+        $products = Product::all();
+        return response()->json($products, 200);
+    }
+    public function store(Request $request){
+        $product = Product::create([
+            'product_name' => $request->product_name,
+            'price' => $request->price,
+            'size' => $request->size]);
+            return response()->json($product, 201);
+    }
+    public function update(Request $request, $id){
+        $product = Product::findOrFail($id);
+        $product->update([
+            'product_name' => $request->product_name,
+            'price' => $request->price,
+            'size' => $request->size
+            ]);
+        return response()->json($product, 200);
+    }
+    public function destroy($id){
+        $product = Product::findOrFail($id);
+        $product->delete();
+        return response()->json(['message' => 'Product deleted successfully'], 200);
+    }
+}
